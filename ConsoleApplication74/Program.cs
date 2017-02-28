8fcef8995dbf497c950a8b757ddb61fb166a8c29
MFothergill <Fothergill1981@gmail.com>
2017-02-28 17:09:47 -0500

    first commit

---
+++ ConsoleApplication74/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication74
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            Console.WriteLine("Hello! welcome to the game called: Raise the Widget.\n Please enter the desired age, gender,and coatcolor you would like for your cat...");
+            // Calling Properties
+            Console.WriteLine("Please enter the age (a number between 1 and 12) first.");
+            
+            Virtual_Pet pet = new Virtual_Pet();
+            
+            pet.Age = int.Parse(Console.ReadLine());
+          
+           
+            
+            Console.WriteLine("Very good.Now enter the desired gender.");
+
+            
+            pet.Gender = Console.ReadLine();
+
+            Console.WriteLine("Thank you.Now please the deisred coat color.");
+
+            pet.Coatcolor = Console.ReadLine();
+
+            //Displaying results of user choices
+            Console.WriteLine("Very good! your cat is: {0}", pet.Age);
+            Console.WriteLine("The gender is: {0}", pet.Gender);
+            Console.WriteLine("The coat is: {0}", pet.Coatcolor);
+
+            Console.WriteLine("Ok,weve determined what the Widget looks like.Now you must raise it.Good luck!");
+            //actual game code
+            
+
+           
+
+            
+
+            pet.PetStatus();
+            Console.WriteLine();
+            Console.WriteLine();
+            pet.DisplayMenu();
+            Console.WriteLine("\n Please enter a number");
+            bool keepGoing = true;
+            while(keepGoing == true)
+            {
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        pet.petFeed();
+                        pet.Statements();
+                        pet.DisplayMenu();
+                        break;
+                    case 2:
+                        pet.petDrink();
+                        pet.Statements();
+                        pet.DisplayMenu();
+                        break;
+                    case 3:
+                        pet.petPlay();
+                        pet.Statements();
+                        pet.DisplayMenu();
+                        break;
+                    case 4:
+                        pet.petTrain();
+                        pet.Statements();
+                        pet.DisplayMenu();
+                        break;
+                    case 5:
+                        Console.WriteLine("Thanks for playing!");
+                        keepGoing = false;
+                        break;
+                }
+
+            }
+            
+            
+
+        }
+    }
+}
---
+++ ConsoleApplication74/Virtual Pet.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication74
+{
+    class Virtual_Pet
+    {
+        //Fields
+        public int hunger;
+        public int thirst;
+        public int boredom;
+        public int weakness;
+
+        private int age;
+        private string gender;
+        private string coatcolor;
+
+        //Constructors
+        public Virtual_Pet()
+        {
+            this.hunger = 50;
+            this.thirst = 50;
+            this.boredom = 50;
+            this.weakness = 50;
+        }
+        //Properties
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+            
+        }   
+        public string Gender
+        {
+            get { return this.gender; }
+            set { gender = value; }
+            
+            
+        }
+        public string Coatcolor
+        {
+            get { return this.coatcolor; }
+            set { coatcolor = value; }
+        }
+           
+        //Methods
+      public void petFeed()
+        {           
+            Console.WriteLine("Widget hunger{0}",hunger += 10);
+            Console.WriteLine("Widget boredom{0}",boredom -= 5);
+            Console.WriteLine("Widget thirst{0}",thirst -= 5);
+            Console.WriteLine("Widget weakness{0}",weakness -= 5);
+            return;
+        }
+        public void petDrink()
+        {
+            Console.WriteLine("Widget thirst{0}",thirst += 10);
+            Console.WriteLine("Widget weakness{0}",weakness -= 5);
+            Console.WriteLine("Widget hunger {0}",hunger -= 5);
+            Console.WriteLine("Widget boredom {0}",boredom -= 5);
+            return;
+        }
+        public void petPlay()
+        {
+            Console.WriteLine("Widget boredom{0}",boredom+= 10);
+            Console.WriteLine("Widget hunger{0}",hunger -= 5);
+            Console.WriteLine("Widget thirst{0}",thirst -= 5);
+            Console.WriteLine("Widget weakness{0}",weakness -= 5);
+            return;
+        }
+        public void petTrain()
+        {
+            Console.WriteLine("Widget weakness{0}",weakness += 10);
+            Console.WriteLine("Widget thirst{0}",thirst -= 5);
+            Console.WriteLine("Widget hunger{0}",hunger -= 5);
+            Console.WriteLine("Widget boredom{0}",boredom -= 5);
+            
+        }
+        public void PetStatus()
+        {
+            Console.WriteLine("Widget hunger{0}", hunger);
+            Console.WriteLine("Widget thirst{0}", thirst);
+            Console.WriteLine("Widget boredom{0}",boredom);
+            Console.WriteLine("Widget weakness{0}", weakness);
+            
+        }
+        public void DisplayMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Feed the Widget");
+            Console.WriteLine("2.Give the Widget some liquid refreshment");
+            Console.WriteLine("3. Play with the Widget");
+            Console.WriteLine("4. Teach the Widget Karate");
+            Console.WriteLine("5. Quit Game");
+        }
+        public void Statements()
+        {
+            if (hunger >= 30 && hunger <= 40)
+            {
+                Console.WriteLine("The Widget needs food-its hungry");
+            }
+            else if (hunger >= 15 && hunger <= 25)
+            {
+                Console.WriteLine("\t\t\t\t IM STARVING MAN!!! FEEED MEEEE!!!");
+            }
+            else if (thirst >= 30 && thirst <= 40)
+            {
+                Console.WriteLine("The Widget throat is a little parched");
+            }
+            else if (thirst >= 15 && thirst <= 25)
+            {
+                Console.WriteLine("\t\t\t\t COUGH! COUGH! IM THIRSTY!!");
+            }
+            else if(boredom >= 30 && boredom <= 40)
+            {
+                Console.WriteLine("Theres nothing to do man,play with me!!");
+            }
+            else if(boredom >= 15 && boredom <= 25)
+            {
+                Console.WriteLine("\t\t\t\t The Widget is devolving into an amoeba-like state");
+            }   
+            else if(weakness >= 30 && weakness <= 40)
+            {
+                Console.WriteLine("The Widget is getting picked on in deez skreetz");
+            }
+            else if (weakness >=15 && weakness <= 25)
+            {
+                Console.WriteLine("\t\t\t\t The Widget is getting robbed on a regular basis");
+            }
+        }
+    }
+
+       
+}
